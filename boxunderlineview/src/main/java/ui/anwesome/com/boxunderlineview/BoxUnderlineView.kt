79aa62137c635ddffdc3ad2d04a27cbef4df2e39
Anwesh43 <anweshthecool0@gmail.com>
2018-02-10 07:20:29 +0530

    adding BoxUnderlineView class

--- boxunderlineview/src/main/java/ui/anwesome/com/boxunderlineview/BoxUnderlineView.kt
+++ boxunderlineview/src/main/java/ui/anwesome/com/boxunderlineview/BoxUnderlineView.kt
@@ -3,3 +3,16 @@ package ui.anwesome.com.boxunderlineview
 /**
  * Created by anweshmishra on 10/02/18.
  */
+import android.view.*
+import android.content.*
+import android.graphics.*
+
+class BoxUnderlineView(ctx:Context):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        return true
+    }
+}
