d10b34215484cc86dbb78374b0ce79340045fa34
Anwesh43 <anweshthecool0@gmail.com>
2018-02-10 08:21:31 +0530

    adding BoxUnderlineView in Activity

--- app/src/main/java/ui/anwesome/com/kotlinboxunderlineview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinboxunderlineview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinboxunderlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.boxunderlineview.BoxUnderlineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BoxUnderlineView.create(this,5)
     }
 }
